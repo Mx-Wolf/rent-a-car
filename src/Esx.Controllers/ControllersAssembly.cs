f4c0f3b45d5fd3e6e908012e17b6d2ee1d8f905a
Maxim Volkov <mx_rux_net@hotmail.com>
2025-05-24 11:09:31 +0300

    adds controller infrastructure

---
+++ src/Esx.Controllers/ControllersAssembly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esx.Controllers;
+public static class ControllersAssembly
+{
+    public static readonly Assembly Reference = typeof(ControllersAssembly).Assembly;
+}
---
+++ src/Esx.Controllers/ReservationsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esx.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class ReservationsController
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await Task.CompletedTask;
+        return new OkObjectResult(new { status = "Reservations OK" });
+    }
+}
